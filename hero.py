595755d20c1a657e241619182997affe64a52d14
Maksim Klychkov <maxim.klychkov64@gmail.com>
2022-01-11 14:54:07 +0300

    Changing Bullet and hero_constants.py

--- hero.py
+++ hero.py
@@ -1,11 +1,32 @@
 import pygame
+from main import all_sprites
+from hero_constants import *
 
 
 class Hero(pygame.sprite.Sprite):
     image = pygame.Surface((20, 20))
     image.fill(pygame.Color('red'))
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *groups):
+        super().__init__(*groups)
         self.image = Hero.image
         self.rect = self.image.get_rect()
+
+
+class Bullet(pygame.sprite.Sprite):
+    image = pygame.Surface((5, 5))
+    image.fill(pygame.Color('green'))
+
+    def __init__(self, *groups, coords):
+        super().__init__(*groups)
+        self.image = Bullet.image
+        self.rect = self.image.get_rect()
+        self.x, self.y = coords
+        self.ticks = 0
+
+    def update(self, *args, **kwargs) -> None:
+        if self.ticks >= BULLET_SPEED:
+            self.ticks = 0
+            self.rect.move(0, -1)
+        else:
+            self.ticks += 1
