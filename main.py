0bb0793172df03911fc8e46873d415aa2b99eda4
Maksim Klychkov <maxim.klychkov64@gmail.com>
2022-01-11 17:01:09 +0300

    Smooth moving of hero

--- main.py
+++ main.py
@@ -15,17 +15,28 @@ clock = pygame.time.Clock()
 hero = hero.Hero()
 zomb1 = zombies.Zombie()
 zomb2 = zombies.ZombieFather()
+key_left = False
+key_right = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                hero.left()
+                key_left = True
             if event.key == pygame.K_RIGHT:
-                hero.right()
+                key_right = True
             if event.key == pygame.K_SPACE:
                 hero.shoot()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                key_left = False
+            if event.key == pygame.K_RIGHT:
+                key_right = False
+    if key_left:
+        hero.left()
+    if key_right:
+        hero.right()
 
     all_sprites.update()
 
