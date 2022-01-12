b35eb3c408bb89bc6eacb48cf6e98ff29c3f0715
Maksim Klychkov <maxim.klychkov64@gmail.com>
2022-01-12 21:33:24 +0300

    Size of cursor and mouse visible settings

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
 
 class Cursor(pygame.sprite.Sprite):
-    image = load_image('cursor.png')
+    image = load_image('cursor.png', -1)
 
     def __init__(self, *groups):
         super().__init__(*groups)
@@ -25,6 +25,7 @@ def show_menu(screen: pygame.Surface):
     # fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     fon = pygame.Surface((WIDTH, HEIGHT))
     fon.fill(pygame.Color('blue'))
+    pygame.mouse.set_visible(False)
     screen.blit(fon, (0, 0))
     cursor_group = pygame.sprite.Group(Cursor())
     cursor_coords = (0, 0)
@@ -35,7 +36,7 @@ def show_menu(screen: pygame.Surface):
             if event.type == pygame.MOUSEMOTION:
                 if pygame.mouse.get_focused():
                     cursor_coords = event.pos
-        screen.fill((0, 0, 0))
+        screen.blit(fon, (0, 0))
         cursor_group.update(cursor_coords)
         cursor_group.draw(screen)
         pygame.display.flip()
