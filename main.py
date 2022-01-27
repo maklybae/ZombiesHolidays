c71c6203e29afab399b8e3d30be7e24109556e7b
Maksim Klychkov <maxim.klychkov64@gmail.com>
2022-01-27 23:46:56 +0300

    select_lvl_in_menu func

--- main.py
+++ main.py
@@ -17,10 +17,8 @@ from gameover import gameover
 
 pygame.display.set_caption('ZombiesHolidays')
 
-
-if (lvl := show_menu(screen)) is None:
-    lvl = load_lastlvl()
-spawn_time, spawn_zombie, spawn_coords = load_level(lvl)
+lvl = -1
+spawn_time, spawn_zombie, spawn_coords = [], [], []
 
 clock = pygame.time.Clock()
 hero = hero.Hero()
@@ -28,6 +26,38 @@ key_left = False
 key_right = False
 ticks = 0
 background = pygame.transform.scale(load_image('background.png'), SIZE)
+
+
+def select_lvl_in_menu(special_flag=None):
+    global lvl, spawn_zombie, spawn_coords, spawn_time, key_left, key_right, ticks
+    if special_flag is not None:
+        if special_flag == 'first_boot':
+            tmp = show_menu(screen)
+            if tmp is None:
+                lvl = load_lastlvl()
+            else:
+                lvl = tmp
+        if special_flag == 'lose' or special_flag == 'win':
+            key_left, key_right = False, False
+            if special_flag == 'lose':
+                gameover(screen)
+            elif special_flag == 'win':
+                win(screen)
+                save_lastlvl(lvl + 1)
+            tmp = show_menu(screen)
+            if tmp is not None:
+                lvl = tmp
+            else:
+                lvl = load_lastlvl()
+            remove_all_sprites()
+            hero.first_position()
+            hero.reset_bullets()
+            ticks = 0
+        spawn_time, spawn_zombie, spawn_coords = load_level(lvl)
+
+
+select_lvl_in_menu('first_boot')
+
 
 while True:
     for event in pygame.event.get():
@@ -63,35 +93,14 @@ while True:
 
     for zombie in zombies_group.sprites():
         if zombie.check_gameover():
-            key_left, key_right = False, False
-            gameover(screen)
-            tmp = show_menu(screen)
-            if tmp is not None:
-                lvl = tmp
-            spawn_time, spawn_zombie, spawn_coords = load_level(lvl)
-            remove_all_sprites()
-            hero.first_position()
-            hero.reset_bullets()
-            ticks = 0
+            select_lvl_in_menu('lose')
 
     ticks += DELTA_TICKS
     if spawn_time and ticks >= spawn_time[0]:
         eval(f'zombies.{spawn_zombie.pop(0)}({spawn_coords.pop(0)})')
         spawn_time.pop(0)
     if not spawn_time and not zombies_group.sprites():
-        key_left, key_right = False, False
-        win(screen)
-        save_lastlvl(lvl + 1)
-        tmp = show_menu(screen)
-        if tmp is not None:
-            lvl = tmp
-        else:
-            lvl = load_lastlvl()
-        spawn_time, spawn_zombie, spawn_coords = load_level(lvl)
-        remove_all_sprites()
-        hero.first_position()
-        hero.reset_bullets()
-        ticks = 0
+        select_lvl_in_menu('win')
     if key_left:
         hero.left()
     if key_right:
